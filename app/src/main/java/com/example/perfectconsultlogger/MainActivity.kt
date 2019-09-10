de27281d9a16f93eac217482199521db6c262bd5
Teodor Kostadinov <ilina.pavllova@gmail.com>
2019-09-10 19:55:53 +0300

    determine call type

--- app/src/main/java/com/example/perfectconsultlogger/MainActivity.kt
+++ app/src/main/java/com/example/perfectconsultlogger/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.telephony.TelephonyManager
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/com/example/perfectconsultlogger/PhoneStateReceiver.kt
+++ app/src/main/java/com/example/perfectconsultlogger/PhoneStateReceiver.kt
@@ -3,10 +3,44 @@ package com.example.perfectconsultlogger
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.telephony.TelephonyManager
 import android.util.Log
 
 class PhoneStateReceiver : BroadcastReceiver() {
+
+    private val TAG = "PhoneStateReceiver"
+
+    private var lastState = TelephonyManager.CALL_STATE_IDLE
+
     override fun onReceive(context: Context?, intent: Intent?) {
-        Log.d("PhoneStateReceiver", "receiver start")
+        checkIncoming(intent)
+    }
+
+
+    private fun checkIncoming(intent: Intent?) {
+        val state = intent?.getStringExtra(TelephonyManager.EXTRA_STATE)
+
+        if (lastState == TelephonyManager.CALL_STATE_IDLE) {
+            if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                val number = intent?.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
+                Log.d(TAG, "Incoming number : $number")
+            } else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+                val number = intent?.getStringExtra(Intent.EXTRA_PHONE_NUMBER)
+                Log.d(TAG, "Outgoing number : $number")
+            }
+        }
+        Log.d(TAG, state)
+        lastState = intState(state)
+    }
+
+    private fun intState(state: String?): Int {
+        if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
+            return TelephonyManager.CALL_STATE_OFFHOOK
+        } else if (state.equals(TelephonyManager.EXTRA_STATE_IDLE)) {
+            return TelephonyManager.CALL_STATE_IDLE
+        } else if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+            return TelephonyManager.CALL_STATE_RINGING
+        }
+        return -1
     }
 }
