0f8017bca211ff443bcf55bb008a0fe0dddd5384
Teodor Kostadinov <ilina.pavllova@gmail.com>
2019-09-25 18:22:26 +0300

    get call duration

--- app/src/main/java/com/example/perfectconsultlogger/PhoneStateReceiver.kt
+++ app/src/main/java/com/example/perfectconsultlogger/PhoneStateReceiver.kt
@@ -7,8 +7,12 @@ import android.telephony.TelephonyManager
 import android.util.Log
 import com.example.perfectconsultlogger.data.CallLog
 import com.example.perfectconsultlogger.data.Database
+import java.time.Duration
+import java.time.Instant
 import java.time.LocalDateTime
+import java.time.Period
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 
 class PhoneStateReceiver : BroadcastReceiver() {
@@ -40,7 +44,6 @@ class PhoneStateReceiver : BroadcastReceiver() {
             } else if (state.equals(TelephonyManager.EXTRA_STATE_OFFHOOK)) {
                 //TODO: extract outgoing call target_number
                 val number = intent?.getStringExtra(Intent.EXTRA_PHONE_NUMBER)
-                Log.d(TAG, "Outgoing target_number : $number " + intent?.action)
             }
         }
         Log.d(TAG, state)
@@ -49,14 +52,35 @@ class PhoneStateReceiver : BroadcastReceiver() {
 
     private fun processIncomingCall(intent: Intent?) {
         val targetNumber = intent?.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER)
-        val formatted = obtainTime()
-        val callLog = targetNumber?.let { CallLog("" ,it, formatted, "10m 12s",true) }
+        val startTime = getStartTime()
+        val duration = getDuration(intent)
+        val callLog = targetNumber?.let { CallLog("", it, startTime, duration.toString(), true) }
         if (callLog != null) {
             database?.insertCallLog(callLog)
         }
     }
 
-    private fun obtainTime(): String {
+    private fun getDuration(intent: Intent?): Int {
+        var state: String?
+        val delay = 1000
+        val period = 1000
+        val timer = Timer()
+        var durationInSeconds = 0
+
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                state = intent?.getStringExtra(TelephonyManager.EXTRA_STATE)
+                Log.d(TAG,"getDuration: $state")
+
+                if(state == TelephonyManager.EXTRA_STATE_OFFHOOK) {
+                    durationInSeconds++
+                }
+            }
+        }, delay.toLong(), period.toLong())
+        return durationInSeconds
+    }
+
+    private fun getStartTime(): String {
         //TODO: check phone states and obtain start and end time
         val current = LocalDateTime.now()
         val formatter = DateTimeFormatter.ofPattern("HH:mm")
