91ee34d74579894b0cf425d3ac573a1e6caec642
Teodor Kostadinov <teodor.kk.93@gmail.com>
2019-10-13 21:22:25 +0300

    Compact permissions intp one

--- app/src/main/java/com/example/perfectconsultlogger/PhoneStateReceiver.kt
+++ app/src/main/java/com/example/perfectconsultlogger/PhoneStateReceiver.kt
@@ -1,50 +1,78 @@
 package com.example.perfectconsultlogger
 
+import android.Manifest
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.provider.CallLog
+import android.support.v4.content.ContextCompat
 import android.telephony.TelephonyManager
-import android.util.Log
+import com.example.perfectconsultlogger.data.CallDetails
 import com.example.perfectconsultlogger.data.Database
-import com.example.perfectconsultlogger.data.Settings
-import java.util.*
+import kotlin.collections.ArrayList
 
 class PhoneStateReceiver : BroadcastReceiver() {
 
     private val TAG = "PhoneStateReceiver"
 
-    private var lastState = TelephonyManager.CALL_STATE_IDLE
-    private var database: Database? = null
-
     override fun onReceive(context: Context?, intent: Intent?) {
-        intent?.let {
-            if(it.action == "android.intent.action.PHONE_STATE" || it.action == "android.intent.action.NEW_OUTGOING_CALL") {
-                database = context?.let { Database.getInstance(it) }
-
-                if (lastState == TelephonyManager.CALL_STATE_IDLE) { //Phone call has ended
-
+        val nonNullIntent = intent ?: return
+        val nonNullContext = context ?: return
+        val database = Database.getInstance(nonNullContext)
+        if (nonNullIntent.action == "android.intent.action.PHONE_STATE" || nonNullIntent.action == "android.intent.action.NEW_OUTGOING_CALL") {
+            //TODO read state if (state == TelephonyManager.CALL_STATE_IDLE) { //Phone call has ended
+                if (ContextCompat.checkSelfPermission(nonNullContext, Manifest.permission.READ_CALL_LOG) == PackageManager.PERMISSION_GRANTED) {
+                    syncUnsyncedCalls(database, nonNullContext)
+                } else {
+                    //TODO notify server for no permission granted
                 }
-            }
+//            }
         }
+
     }
 
-    private fun getCallDetails(context: Context): String {
-        val sb = StringBuffer();
-        val managedCursor = context.managedQuery(CallLog.Calls.CONTENT_URI, null, null, null, android.provider.CallLog.Calls.DATE + " DESC limit 2;");
+    private fun syncUnsyncedCalls(
+        database: Database,
+        nonNullContext: Context
+    ) {
+        database.getLastSyncedCallTimestamp(object : Database.DataListener<Long> {
+            override fun onData(lastSyncedCallTimestamp: Long) {
+                val unsyncedCalls = getUnsyncedCalls(nonNullContext, lastSyncedCallTimestamp)
+                var latestSyncedCallTimestamp = 0L
+                for (call in unsyncedCalls) {
+                    if (syncCall(call)) {
+                        latestSyncedCallTimestamp = call.callStartTimestamp
+                    } else {
+                        break
+                    }
+                }
+                database.setLastSyncedCallTimestamp(latestSyncedCallTimestamp)
+            }
+        })
+    }
+
+    private fun syncCall(call: CallDetails): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun getUnsyncedCalls(context: Context, sinceTimestamp: Long): List<CallDetails> {
+        val unsyncedCalls = ArrayList<CallDetails>()
+        val managedCursor = context.contentResolver.query(
+            CallLog.Calls.CONTENT_URI,
+            null,
+            android.provider.CallLog.Calls.DATE + " >= ?",
+            arrayOf(sinceTimestamp.toString()),
+            android.provider.CallLog.Calls.DATE
+        );
         val number = managedCursor.getColumnIndex(CallLog.Calls.NUMBER);
         val type = managedCursor.getColumnIndex(CallLog.Calls.TYPE);
         val date = managedCursor.getColumnIndex(CallLog.Calls.DATE);
         val duration = managedCursor.getColumnIndex(CallLog.Calls.DURATION);
-        sb.append("Call Log :");
+
         while (managedCursor.moveToNext()) {
-            val phNumber = managedCursor.getString(number);
-            val callType = managedCursor.getString(type);
-            val callTimestampText = managedCursor.getString(date);
-            val callDate = Date(callTimestampText.toLong())
-            val callDuration = managedCursor.getString(duration);
-            var callTypeText: String? = null
-            when (Integer.parseInt(callType)) {
+            var callTypeText = ""
+            when (Integer.parseInt(managedCursor.getString(type))) {
                 CallLog.Calls.OUTGOING_TYPE -> callTypeText = "OUTGOING";
                 CallLog.Calls.INCOMING_TYPE -> callTypeText = "INCOMING";
                 CallLog.Calls.MISSED_TYPE -> callTypeText = "MISSED";
@@ -53,10 +81,16 @@ class PhoneStateReceiver : BroadcastReceiver() {
                 CallLog.Calls.BLOCKED_TYPE -> callTypeText = "BLOCKED";
                 CallLog.Calls.ANSWERED_EXTERNALLY_TYPE -> callTypeText = "EXTERNALLY_ANSWERED";
             }
-            sb.append("\nPhone Number:--- " + phNumber + " \nCall Type:--- " + callTypeText + " \nCall Date:--- " + callDate + " \nCall duration in sec :--- " + callDuration);
-            sb.append("\n----------------------------------");
+            unsyncedCalls.add(
+                CallDetails(
+                    managedCursor.getString(number),
+                    callTypeText,
+                    managedCursor.getString(date).toLong(),
+                    managedCursor.getString(duration).toLong()
+                )
+            )
         }
-        return sb.toString()
+        return unsyncedCalls
     }
 
 }
--- app/src/main/java/com/example/perfectconsultlogger/data/Database.kt
+++ app/src/main/java/com/example/perfectconsultlogger/data/Database.kt
@@ -22,45 +22,49 @@ class Database(context: Context) {
     private val database: AppDatabase = Room.databaseBuilder(context, AppDatabase::class.java, "app-db")
         .fallbackToDestructiveMigration().build()
 
-    fun getLastSyncedCallTimestamp(listener: DataListener<Long>){
-        SettingRetrieveTask(Settings.OWNER_PHONE, database, object: DataListener<Settings> {
-            override fun onData(settings: Settings) {
-                listener.onData(settings.value.toLong())
-            }
-        } ).execute()
-    }
-
-    fun setLastSyncedCallTimestamp(value: Long){
-        val ownerPhone = Settings(Settings.OWNER_PHONE, value.toString())
-        SettingsInsertTask(database).execute(ownerPhone)
-    }
-
-    fun getOwnerPhone(listener: DataListener<String>){
-        SettingRetrieveTask(Settings.OWNER_PHONE, database, object: DataListener<Settings> {
-            override fun onData(settings: Settings) {
-                listener.onData(settings.value)
+    fun getLastSyncedCallTimestamp(listener: DataListener<Long>) {
+        SettingRetrieveTask(Settings.OWNER_PHONE, database, object : DataListener<Settings?> {
+            override fun onData(settings: Settings?) {
+                listener.onData(settings?.value?.toLong() ?: 0L)
             }
         }).execute()
     }
 
-    fun setOwnerPhone(value: String){
+    fun setLastSyncedCallTimestamp(value: Long) {
+        val ownerPhone = Settings(Settings.OWNER_PHONE, value.toString())
+        SettingsInsertTask(database).execute(ownerPhone)
+    }
+
+    fun getOwnerPhone(listener: DataListener<String>) {
+        SettingRetrieveTask(Settings.OWNER_PHONE, database, object : DataListener<Settings?> {
+            override fun onData(settings: Settings?) {
+                listener.onData(settings?.value ?: "")
+            }
+        }).execute()
+    }
+
+    fun setOwnerPhone(value: String) {
         val ownerPhone = Settings(Settings.OWNER_PHONE, value)
         SettingsInsertTask(database).execute(ownerPhone)
     }
 
-    private class SettingsInsertTask(val database: AppDatabase): AsyncTask<Settings, Void, Void>(){
+    private class SettingsInsertTask(val database: AppDatabase) : AsyncTask<Settings, Void, Void>() {
         override fun doInBackground(vararg params: Settings?): Void? {
             params[0]?.let { database.settingsDao().insert(it) }
             return null
         }
     }
 
-    private class SettingRetrieveTask(val setting: String, val database: AppDatabase, val listener: DataListener<Settings>) : AsyncTask<Void, Void, Settings>(){
-        override fun doInBackground(vararg params: Void?): Settings {
+    private class SettingRetrieveTask(
+        val setting: String,
+        val database: AppDatabase,
+        val listener: DataListener<Settings?>
+    ) : AsyncTask<Void, Void, Settings?>() {
+        override fun doInBackground(vararg params: Void?): Settings? {
             return database.settingsDao().getSetting(setting)
         }
 
-        override fun onPostExecute(phoneNumber: Settings) {
+        override fun onPostExecute(phoneNumber: Settings?) {
             super.onPostExecute(phoneNumber)
             listener.onData(phoneNumber)
         }
